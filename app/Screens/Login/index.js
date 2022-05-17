7fbbbef050b92c0946d35d19a7ca2445af4ffb31
Sruthi Vijayan <sruthi.vijayan@techversantinfo.com>
2022-05-17 15:12:21 +0530

    login

---
+++ app/Screens/Login/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class Login extends Component {
+  constructor(props) {
+    this.super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <View>
+        <Text>Sign In</Text>
+      </View>
+    );
+  }
+}
